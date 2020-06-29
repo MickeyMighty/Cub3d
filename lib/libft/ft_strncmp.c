90c6e46480fbdfc50c2ce85870b62aa95c28968d
Loamar <loamar@e1r11p14.42.fr>
2020-06-29 16:00:03 +0200

    cub3d

--- lib/libft/ft_strncmp.c
+++ lib/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: loamar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:21:37 by loamar            #+#    #+#             */
-/*   Updated: 2019/11/29 16:21:39 by loamar           ###   ########.fr       */
+/*   Updated: 2020/06/29 15:59:56 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (n != 0)
-	{
-		i = 0;
-		while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
-		{
-			if ((unsigned char)s1[i] != (unsigned char)s2[i])
-				return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-			i++;
-		}
-	}
-	return (0);
+	i = 0;
+	if (!n)
+		return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < (n - 1))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
